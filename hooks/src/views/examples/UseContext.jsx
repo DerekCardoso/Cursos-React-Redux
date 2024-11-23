bb8b8f234bb85d675b886498e0a9ca6a1ec323bf
Derek Cardoso <derek.cardoso@hotmail.com>
2024-11-23 17:01:39 -0300

    termina sessão 5

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 
 import DataContext from "../../data/DataContext";
+import SectionTitle from "../../components/layout/SectionTitle";
+import AppContext from "../../data/Store";
 
 const UseContext = (props) => {
   const { state, setState } = useContext(DataContext);
@@ -13,6 +15,7 @@ const UseContext = (props) => {
     });
   }
 
+  const { number, setNumber } = useContext(AppContext);
   return (
     <div className="UseContext">
       <PageTitle
@@ -20,6 +23,7 @@ const UseContext = (props) => {
         subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
       />
 
+      <SectionTitle title="Exercicio #01" />
       <div className="center">
         <span className="text">{state.text}</span>
         <span className="text">{state.number}</span>
@@ -29,6 +33,19 @@ const UseContext = (props) => {
             -1
           </button>
           <button className="btn" onClick={() => addNumber(+1)}>
+            +1
+          </button>
+        </div>
+      </div>
+
+      <SectionTitle title="Exercicio #02" />
+      <div className="center">
+        <spam className="text">{number}</spam>
+        <div>
+          <button className="btn" onClick={() => setNumber(number - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setNumber(number + 1)}>
             +1
           </button>
         </div>
--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
+
+import { useCounter } from "../../hooks/useCounter";
+import { useFetch } from "../../hooks/useFetch";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseCustom">
-            <PageTitle
-                title="Seu Hook"
-                subtitle="Vamos aprender como criar o nosso próprio Hook!"
-            />
-        </div>
-    )
-}
+  const [count, inc, dec] = useCounter();
+  const url = "https://files.coder.com.br/curso-react/estados.json";
+  const response = useFetch(url);
 
-export default UseRef
+  function showStates(states) {
+    return states.map((state) => (
+      <li>
+        {state.nome} - {state.sigla}
+      </li>
+    ));
+  }
+  // https://files.coder.com.br/curso-react/estados.json
+  //   fetch("https://files.cod3r.com.br/curso-react/estados.json")
+  //     .then((resp) => resp.json())
+  //     .then((json) => console.log(json));
+
+  return (
+    <div className="UseCustom">
+      <PageTitle
+        title="Seu Hook"
+        subtitle="Vamos aprender como criar o nosso próprio Hook!"
+      />
+
+      <SectionTitle title="Exercício 01" />
+
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => inc()}>
+            +1
+          </button>
+          <button className="btn" onClick={() => dec()}>
+            -1
+          </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercício 02" />
+      <div className="center">
+        <ul>{response.data ? showStates(response.data) : false}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default UseRef;
